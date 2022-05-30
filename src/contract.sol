96bfe07b0091136e55106c17d198107894976918
shimizu-atsushi <shimizu-atsushi@PC-0003405.local>
2022-05-30 20:21:11 +0900

    add solidity

---
+++ src/contract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+
+contract ColorfulCandy is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("ColorfulCandy", "CC") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://raw.githubusercontent.com/432ats/test-contract/main/meta/";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
